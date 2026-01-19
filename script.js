9d9abb0d71ef5149675cef21bf695b5f8a679876
Duarte8754 <duarteosvaldod@gmail.com>
2026-01-19 10:33:07 +0200

    Remove unused variable 'usuarioTipo'
    Removed unused variable 'usuarioTipo'.

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-let usuarioTipo = null;
-
 // ===== NAVEGAÇÃO =====
 function mostrarInscricao(){ mostrarPagina('inscricao'); }
 function mostrarLogin(){ mostrarPagina('login'); }
